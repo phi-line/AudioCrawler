7677ac316dc02eb86a787853c87f2fed70b1338e
cgarduno1garduno <garduno.cris@gmail.com>
2017-04-02 00:17:04 -0700

    fix ncol in cat_samples

--- cat_samples.py
+++ cat_samples.py
@@ -13,6 +13,7 @@ def cat_samples(master_data):
 	perc  = []
 	harm  = []
 	genre = []
+	n = len(data[0][0])
 
 	# Decompose the data into genre and spec data, for list members (!genre) the dtype is ndarray
 	for data in master_data:
@@ -23,10 +24,10 @@ def cat_samples(master_data):
 
 	# Now we have lists of either 1D arrays (mel, perc, harm) or str (genre)
 	# Next we convert each list to a ndarray
-	mel   = vec2matrix(mel,  ncol=len(master_data))
-	perc  = vec2matrix(perc, ncol=len(master_data))
-	harm  = vec2matrix(harm, ncol=len(master_data))
-	genre = vec2matrix(genre,ncol=len(master_data))
+	mel   = vec2matrix(mel,  ncol=n)
+	perc  = vec2matrix(perc, ncol=n)
+	harm  = vec2matrix(harm, ncol=n)
+	genre = vec2matrix(genre,ncol=n)
 
 	data = ((mel, perc, harm), genre)
 	return data
